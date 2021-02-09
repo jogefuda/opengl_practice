e2545377ece7fc202d0d6a0cdaf8b47027c604ef
jogefuda <71481387+jogefuda@users.noreply.github.com>
2021-02-09 18:03:50 +0800

    add glm and license

---
+++ include/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /tmp/Downloads/glm/glm
+# Build directory: /tmp/Downloads/glm/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
